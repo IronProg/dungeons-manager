74dae15b58232267b069b96173135c1c2301deb6
IronProg <rnberny1@gmail.com>
2026-01-21 20:38:44 -0300

    add en locale

--- i18n/index.ts
+++ i18n/index.ts
@@ -1,13 +1,11 @@
 import { I18n } from 'i18n-js';
 import * as Localization from 'expo-localization';
 import { pt } from './locales/pt';
+import { en } from './locales/en';
 
 const i18n = new I18n({
   pt,
-  en: {
-    save: 'Save',
-    cancel: 'Cancel',
-  },
+  en,
 });
 
 i18n.locale = Localization.getLocales()[0]?.languageCode ?? 'pt';
