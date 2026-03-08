b9aa7f4878eff26812c16c400f51e48eb15ea0e9
IronProg <rnberny1@gmail.com>
2026-03-08 01:07:25 -0300

    update equipments keyboard

--- app/(authenticated)/(drawer)/(tabs)/equipments.tsx
+++ app/(authenticated)/(drawer)/(tabs)/equipments.tsx
@@ -4,18 +4,21 @@ import { Container } from 'components/Container';
 import { View } from 'react-native';
 import { Notes } from 'components/Notes/Notes';
 import { Equipments } from 'components/Equipments/Equipments';
-import { ScrollView } from 'react-native-gesture-handler';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-controller';
 
 export default function CharacterSheetScreen() {
   return (
     <Container>
-      <ScrollView>
+      <KeyboardAwareScrollView
+        className="flex-1"
+        contentContainerClassName="grow gap-4 flex flex-col p-4"
+      >
         <View className="px-2 flex-col gap-6 flex items-stretch">
           <Equipments />
 
           <Notes />
         </View>
-      </ScrollView>
+      </KeyboardAwareScrollView>
     </Container>
   );
 }
